3c43444539e6df7d2c73670e2d8a8e3569a23a17
Ange Yaghi <ange.yaghi@outlook.com>
2019-12-04 13:32:26 -0500

    Initial commit

---
+++ include/example_header.h
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_HEADER_H
+#define EXAMPLE_HEADER_H
+
+// Just an example header file
+
+#endif /* EXAMPLE_HEADER_H */
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "../include/example_header.h"
+
+#include <iostream>
+
+int main() {
+	std::cout << "Empty project" << std::endl;
+
+	return 0;
+}
