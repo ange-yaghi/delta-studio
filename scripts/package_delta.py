c4c82b4ca08acaec631d6c55f43e42a52bfbbdb2
Ange Yaghi <ange.yaghi@outlook.com>
2020-03-23 01:28:42 -0400

    [#4] : Updated packaging script

--- scripts/package_delta.py
+++ scripts/package_delta.py
@@ -42,18 +42,18 @@ RESOURCES = [
     package_build.Resource('fonts', '{root}/engines/basic/fonts', '{output}/engines/basic/fonts'),
     package_build.Resource('shaders', '{root}/engines/basic/shaders', '{output}/engines/basic/shaders'),
     package_build.Resource('binaries', '{vs_build_output}/delta-basic-engine.lib', '{output}/lib/{architecture}/{configuration}', resource_type='file'),
-    package_build.Resource('binaries', '{vs_build_output}/delta-basic-engine.pdb', '{output}/lib/{architecture}/{configuration}', resource_type='file'),
+    package_build.Resource('binaries', '{vs_build_output}/delta-basic-engine.pdb', '{output}/lib/{architecture}/{configuration}', resource_type='file', optional=True),
 
     # Physics engine
     package_build.Resource('headers', '{root}/physics/include', '{output}/physics/include'),
     package_build.Resource('binaries', '{vs_build_output}/delta-physics.lib', '{output}/lib/{architecture}/{configuration}', resource_type='file'),
-    package_build.Resource('binaries', '{vs_build_output}/delta-physics.pdb', '{output}/lib/{architecture}/{configuration}', resource_type='file'),
+    package_build.Resource('binaries', '{vs_build_output}/delta-physics.pdb', '{output}/lib/{architecture}/{configuration}', resource_type='file', optional=True),
 
     # Core
     package_build.Resource('headers', '{root}/include', '{output}/include'),
     package_build.Resource('runtime dependencies', '{dll_dir}/', '{output}/runtime/{architecture}', resource_type='dlls'),
     package_build.Resource('binaries', '{vs_build_output}/delta-core.lib', '{output}/lib/{architecture}/{configuration}', resource_type='file'),
-    package_build.Resource('binaries', '{vs_build_output}/delta-core.pdb', '{output}/lib/{architecture}/{configuration}', resource_type='file'),
+    package_build.Resource('binaries', '{vs_build_output}/delta-core.pdb', '{output}/lib/{architecture}/{configuration}', resource_type='file', optional=True),
     package_build.Resource('visual studio redist', '{vc_crt_path}', '{output}/runtime/{architecture}', resource_type='dlls')
 ]
 
