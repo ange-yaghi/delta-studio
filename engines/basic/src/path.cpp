3aad448435ef22f1291c2eeb0c848a0afb6cbee1
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-13 00:16:58 +0100

    Use C++17's filesystem header if we can
    This avoids having to find and build boost which is always a nice thing
    to skip.

--- engines/basic/src/path.cpp
+++ engines/basic/src/path.cpp
@@ -1,6 +1,8 @@
 #include "../include/path.h"
 
+#if !USE_CXX17_FILESYSTEM
 #include <boost/filesystem.hpp>
+#endif // USE_CXX17_FILESYSTEM
 
 dbasic::Path::Path(const std::string &path) {
     m_path = nullptr;
@@ -13,16 +15,16 @@ dbasic::Path::Path(const char *path) {
 }
 
 dbasic::Path::Path(const Path &path) {
-    m_path = new boost::filesystem::path;
-    *m_path = path.GetBoostPath();
+    m_path = new filesystem::path;
+    *m_path = path.GetPath();
 }
 
 dbasic::Path::Path() {
     m_path = nullptr;
 }
 
-dbasic::Path::Path(const boost::filesystem::path &path) {
-    m_path = new boost::filesystem::path;
+dbasic::Path::Path(const filesystem::path &path) {
+    m_path = new filesystem::path;
     *m_path = path;
 }
 
@@ -37,27 +39,27 @@ std::string dbasic::Path::ToString() const {
 void dbasic::Path::SetPath(const std::string &path) {
     if (m_path != nullptr) delete m_path;
 
-    m_path = new boost::filesystem::path(path);
+    m_path = new filesystem::path(path);
 }
 
 bool dbasic::Path::operator==(const Path &path) const {
-    return boost::filesystem::equivalent(this->GetBoostPath(), path.GetBoostPath());
+    return filesystem::equivalent(this->GetPath(), path.GetPath());
 }
 
 dbasic::Path dbasic::Path::Append(const Path &path) const {
-    return Path(GetBoostPath() / path.GetBoostPath());
+    return Path(GetPath() / path.GetPath());
 }
 
 void dbasic::Path::GetParentPath(Path *path) const {
-    path->m_path = new boost::filesystem::path;
+    path->m_path = new filesystem::path;
     *path->m_path = m_path->parent_path();
 }
 
 const dbasic::Path &dbasic::Path::operator=(const Path &b) {
     if (m_path != nullptr) delete m_path;
 
-    m_path = new boost::filesystem::path;
-    *m_path = b.GetBoostPath();
+    m_path = new filesystem::path;
+    *m_path = b.GetPath();
 
     return *this;
 }
@@ -71,7 +73,7 @@ std::string dbasic::Path::GetStem() const {
 }
 
 dbasic::Path dbasic::Path::GetAbsolute() const {
-    return boost::filesystem::absolute(*m_path);
+    return filesystem::absolute(*m_path);
 }
 
 bool dbasic::Path::IsAbsolute() const {
@@ -79,5 +81,5 @@ bool dbasic::Path::IsAbsolute() const {
 }
 
 bool dbasic::Path::Exists() const {
-    return boost::filesystem::exists(*m_path);
+    return filesystem::exists(*m_path);
 }
