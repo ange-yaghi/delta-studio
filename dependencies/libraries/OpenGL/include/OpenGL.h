84ed9623984ce56a092231f462940667c50c72b6
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-13 01:12:49 +0100

    Correct header case
    Also remove Windows.h since the headers include them anyway if they're
    required on your platform.

--- dependencies/libraries/OpenGL/include/OpenGL.h
+++ dependencies/libraries/OpenGL/include/OpenGL.h
@@ -1,9 +1,12 @@
 #ifndef OPENGL_H
 #define OPENGL_H
 
-#include <Windows.h>
-#include <gl/gl.h>
-#include <gl/glext.h>
+
+#include <GL/gl.h>
+#include <GL/glext.h>
+
+#if !__linux__
 #include <gl/wglext.h>
+#endif
 
 #endif /* OPENGL_H */
