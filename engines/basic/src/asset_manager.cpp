1b82a5fafd78fb68d4f629d29c741c9efcedd5b3
bobsayshilol <bobsayshilol@live.co.uk>
2022-08-16 17:05:35 +0100

    Move Windows specific header out of core.h
    Any use cases of this should use the type required, though wav reading
    is simple enough to be generic for all platforms.

--- engines/basic/src/asset_manager.cpp
+++ engines/basic/src/asset_manager.cpp
@@ -3,6 +3,10 @@
 #include "../include/animation_export_file.h"
 #include "../include/delta_engine.h"
 
+#ifdef _WIN32
+#include "../../../include/yds_windows_audio_wav_file.h"
+#endif
+
 #include <set>
 #include <sys/stat.h>
 
@@ -544,6 +548,7 @@ dbasic::TextureAsset *dbasic::AssetManager::GetTexture(const char *name) {
 ysError dbasic::AssetManager::LoadAudioFile(const char *fname, const char *name) {
     YDS_ERROR_DECLARE("LoadAudioFile");
 
+#ifdef _WIN32
     ysWindowsAudioWaveFile waveFile;
     waveFile.OpenFile(fname);
 
@@ -560,6 +565,10 @@ ysError dbasic::AssetManager::LoadAudioFile(const char *fname, const char *name)
     newAsset->SetName(name);
 
     return YDS_ERROR_RETURN(ysError::None);
+#else
+    // TODO
+    return YDS_ERROR_RETURN(ysError::UnsupportedType);
+#endif
 }
 
 dbasic::AudioAsset *dbasic::AssetManager::GetAudioAsset(const char *name) {
